5039467216f21ea70c0d67cdeba72bb202d4f855
Giulia-Caroliny <giuliacacal@gmail.com>
2024-05-20 16:08:25 -0300

    aprendendo CRUD, mini projeto, primeira versão

---
+++ 2º semestre/Métodos e Técnicas de programação (CRUD)/CRUD/LojaTADS-V1/src/lojatads/LojaTADS.java
@@ -0,0 +1,11 @@
+package lojatads;
+import view.frmLogin;
+
+public class LojaTADS {
+
+    public static void main(String[] args) {
+        frmLogin log = new frmLogin();
+        log.setVisible(true);
+    }
+    
+}
---
+++ 2º semestre/Métodos e Técnicas de programação (CRUD)/CRUD/LojaTADS-V1/src/model/Usuario.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Usuario {
+
+    public int id;
+    public String nome;
+    public String email;
+    public String senha;
+
+    public void cadastrarUsuario(int i, String n, String e, String s) {
+        id = i;
+        nome = n;
+        email = e;
+        senha = s;
+    }
+
+}
