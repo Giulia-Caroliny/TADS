ed4fbe756d06c6a33a8bfda49358c949e7ca3c94
Giulia-Caroliny <giuliacacal@gmail.com>
2024-06-24 11:12:26 -0300

    Lista IV: classes Usuario e Revista

---
+++ 3° semestre/POO/refazendo algoritimos com POO/New Folder/biblioteca/src/entities/Revista.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author giuli
+ */
+public class Revista extends ItemBiblioteca {
+
+    private String mancheteCapa;
+    private String mesAno;
+
+    public String getMancheteCapa() {
+        return mancheteCapa;
+    }
+
+    public void setMancheteCapa(String mancheteCapa) {
+        this.mancheteCapa = mancheteCapa;
+    }
+
+    public String getMesAno() {
+        return mesAno;
+    }
+
+    public void setMesAno(String mesAno) {
+        this.mesAno = mesAno;
+    }
+
+    @Override
+    public void exibir() {
+        System.out.println("Revista: "
+                + mancheteCapa
+                + "/nMês/ano:"
+                + mesAno);
+    }
+}
---
+++ 3° semestre/POO/refazendo algoritimos com POO/New Folder/biblioteca/src/entities/Usuario.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author giuli
+ */
+public class Usuario {
+    
+    private String nome;
+    private String matricula;
+    private int idade;
+
+    public Usuario(String nome, String matricula, int idade) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
