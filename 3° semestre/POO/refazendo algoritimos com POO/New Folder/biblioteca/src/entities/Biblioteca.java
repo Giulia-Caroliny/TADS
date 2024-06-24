8a94625d92c38947d5d8a286fd029e855ead0428
Giulia-Caroliny <giuliacacal@gmail.com>
2024-06-24 11:02:32 -0300

    Lista IV: pacote entities, classes biblioteca e itemBiblioteca

---
+++ 3° semestre/POO/refazendo algoritimos com POO/New Folder/biblioteca/src/entities/Biblioteca.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author giuli
+ */
+public class Biblioteca {
+
+    private String nome;
+    private String horarioFuncionamento;
+    private List<ItemBiblioteca> itens;
+
+    public Biblioteca(String nome, String horarioFuncionamento) {
+        this.nome = nome;
+        this.horarioFuncionamento = horarioFuncionamento;
+        this.itens = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getHorarioFuncionamento() {
+        return horarioFuncionamento;
+    }
+
+    public void setHorarioFuncionamento(String horarioFuncionamento) {
+        this.horarioFuncionamento = horarioFuncionamento;
+    }
+
+    public boolean inserirItem(ItemBiblioteca item) {
+        return itens.add(item);
+    }
+
+    public ItemBiblioteca consultarItem(String titulo) {
+        for (ItemBiblioteca item : itens) {
+            if (item.getTitulo().equals(titulo)) {
+                return itens.get(itens.indexOf(item));
+            }
+        }
+        return null;
+    }
+
+    public boolean excluirItem(String titulo) {
+        for (ItemBiblioteca item : itens) {
+            if (item.getTitulo().equals(titulo)) {
+                return itens.remove(item);
+            }
+        }
+        return false;
+    }
+}
---
+++ 3° semestre/POO/refazendo algoritimos com POO/New Folder/biblioteca/src/entities/ItemBiblioteca.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author giuli
+ */
+public abstract class ItemBiblioteca {
+
+    private String titulo;
+    private String editora;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public abstract void exibir();
+}
